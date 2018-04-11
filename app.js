ea8d6131848b6cff8b10703bf31f2334679d3929
carlosjayfr <carlosjfr2@hotmail.com>
2018-04-11 10:17:38 +0200

    crud

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ mongoose.connect('mongodb://localhost:27017/erp',{promiseLibrary: require('blueb
             .catch((err)=>{
                 console.error(err);
             });
+//Para eliminar problemas de las cabeceras. No tiene nada que ver con angular ni con Node. Es por seguridad
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
+    next();
+});
 
 app.use(bodyParser.json({}));
 app.use(bodyParser.urlencoded({'extended': false}));
--- routes/proveedor.js
+++ routes/proveedor.js
@@ -15,12 +15,30 @@ app.get('/', (req,res,next)=> {
                 errores: err
             })
         }
-        // res.status(200).json({
-        //     ok: true,
-        //     proveedores: proveedores
-        // });
-        res.status(200).json(proveedores);
+        res.status(200).json({
+            ok: true,
+            proveedores: proveedores
+        });
+       
 
+    });
+
+});
+
+app.get('/:id',function(req,res,next){
+
+    Proveedor.findById(req.params.id,(err,proveedor)=>{
+        if (err){
+            return res.status(500).json({
+                ok: false,
+                mensaje: 'Error acceso DB',
+                errores: err
+            })
+        }
+        res.status(200).json({
+            ok: true,
+            proveedor: proveedor
+        });
     });
 
 });
